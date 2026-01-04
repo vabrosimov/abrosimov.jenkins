ddc92c273b0bc462ff0f1de2d58c7d59b1c4fa32
vladimir <abrosimov.vladimir01@gmail.com>
2026-01-04 17:55:26 +0530

    Application update

--- src/ru/abrosimov/jenkins/context/Application.groovy
+++ src/ru/abrosimov/jenkins/context/Application.groovy
@@ -1,7 +1,9 @@
 package ru.abrosimov.jenkins.context
 
 interface Application {
-    String digest
     final String mavenGroup
     final String mavenArtifact
+    String digest
+    String versionParamName
+    String version
 }
