b9f55c7eb7a1a0899fdfc6fb06a7fb7f80077934
vladimir <abrosimov.vladimir01@gmail.com>
2026-01-04 18:26:06 +0530

    FindDigest stage

--- src/ru/abrosimov/jenkins/context/Application.groovy
+++ src/ru/abrosimov/jenkins/context/Application.groovy
@@ -3,6 +3,7 @@ package ru.abrosimov.jenkins.context
 interface Application {
     final String mavenGroup
     final String mavenArtifact
+    final String image
     final String versionParamName
     String digest
     String version
---
+++ src/ru/abrosimov/jenkins/stages/FindDigest.groovy
@@ -0,0 +1,42 @@
+package ru.abrosimov.jenkins.stages
+
+import ru.abrosimov.jenkins.context.Application
+import ru.abrosimov.jenkins.core.Jenkins
+
+class FindDigest extends Jenkins {
+
+    FindDigest(Object jenkins) {
+        super(jenkins)
+    }
+
+    void call(Application application) {
+        def manifestsUrl = "${jenkins.REGISTRY}/${application.image}/manifests/${jenkins.params[application.versionParamName]}"
+
+        jenkins.withCredentials([
+                jenkins.usernamePassword(
+                        credentialsId: "NEXUS_CREDENTIALS",
+                        usernameVariable: "NEXUS_USER",
+                        passwordVariable: "NEXUS_PASSWORD"
+                )
+        ]) {
+            jenkins.withEnv([
+                    "MANIFESTS_URL=${manifestsUrl}"
+            ]) {
+                String digest = jenkins.sh(
+                        script: '''
+                                curl -s -u $NEXUS_USER:$NEXUS_PASSWORD $MANIFESTS_URL |
+                                jq -r '.manifests[] | select(.platform.architecture=="amd64") | .digest'
+                                ''',
+                        returnStdout: true
+                ).trim()
+
+                if (digest.isEmpty()) {
+                    jenkins.error "No digest found in registry"
+                }
+
+                jenkins.echo "Found digest in registry: ${digest}"
+//                application.setDigest(digest)
+            }
+        }
+    }
+}
